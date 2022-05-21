013db8bcbe492fa147d233ecee80c3f57a45a424
tyustay <darkfodal2002@gmail.com>
2022-05-21 12:17:17 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            double x, y, answer;
+
+            x = Convert.ToDouble(textBox.Text);
+            y = Convert.ToDouble(textBox1.Text);
+
+            answer = x + y;
+
+            textBox2.Text = answer.ToString();
+        }
+    }
+}
